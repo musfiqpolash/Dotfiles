8cfaf9cf5cbee0687a396eeb8a7b5ed15c1b720f
musfiqpolash <musfiqpolash@gmail.com>
2023-05-16 04:39:28 +0200

    configuration copy to ln_files

---
+++ ln_files.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+if [ ! -d "$HOME/.config" ]
+    then mkdir $HOME/.config
+fi
+
+ln -sf $HOME/Dotfiles/i3 $HOME/.config
+ln -sf $HOME/Dotfiles/kitty $HOME/.config
+ln -sf $HOME/Dotfiles/polybar $HOME/.config
+ln -sf $HOME/Dotfiles/scripts $HOME/.config
+ln -sf $HOME/Dotfiles/alacritty $HOME/.config
+ln -sf $HOME/Dotfiles/bspwm $HOME/.config
+ln -sf $HOME/Dotfiles/sxhkd $HOME/.config
+ln -sf $HOME/Dotfiles/.bash* $HOME/
